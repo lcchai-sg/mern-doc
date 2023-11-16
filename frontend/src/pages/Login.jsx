dc5d7a4681e20b6b865cfbdc5ec5c9091b86b8e0
lcchai <lc.chai.sg@gmail.com>
2023-11-16 17:36:34 +0800

    barebone

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className="bg-primary text-white p-3">Login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+    return (
+        <div>
+            <h1 className="bg-primary text-white p-3">Register Page</h1>
+        </div>
+    );
+};
+
+export default Register;
